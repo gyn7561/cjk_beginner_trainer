a19c0c4d4b1499df0268e2055bac718bbef5d4e4
ian gong <200804632@qq.com>
2019-12-15 10:27:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import CharCard from './CharCard';
+import { Button } from '@material-ui/core';
 // import logo from './logo.svg';
 // import './App.css';
 
 function App() {
+  let [charSet, setCharSet] = React.useState(null);
+
+  if (charSet == null) {
+    return <div>
+      <Button onClick={() => setCharSet("kana")}>假名</Button>
+      <Button onClick={() => setCharSet("hangul")} >谚文</Button>
+    </div>;
+  }
+
   return (
     <div>
-      <CharCard charSet="hangul" />
-    <CharCard charSet="kana" />
+      <CharCard charSet={charSet} />
     </div>
   );
 }
