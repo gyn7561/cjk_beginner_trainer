08d94463bd2048d5d7520ab730fce5fa3543863a
ian gong <200804632@qq.com>
2021-12-21 00:11:34 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CharCard from './CharCard';
+import CharCardV2 from './CharCardV2';
 import { Button } from '@material-ui/core';
 // import logo from './logo.svg';
 // import './App.css';
@@ -11,12 +11,13 @@ function App() {
     return <div>
       <Button onClick={() => setCharSet("kana")}>假名</Button>
       <Button onClick={() => setCharSet("hangul")} >谚文</Button>
+      <Button onClick={() => setCharSet("hanja")} >韩语汉字</Button>
     </div>;
   }
 
   return (
     <div>
-      <CharCard charSet={charSet} />
+      <CharCardV2 charSet={charSet} />
     </div>
   );
 }
--- src/CharCardV2.js
+++ src/CharCardV2.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -9,6 +9,8 @@ import { FormControl, InputLabel, OutlinedInput } from "@material-ui/core";
 import hangul from "./hangul";
 import kana from "./kana";
 import * as Hangul from 'hangul-js';
+let lv1Hanja = require("./data/lv1.json");
+let hanjaMap = require("./data/hanja.json");
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -22,9 +24,15 @@ const useStyles = makeStyles(theme => ({
 export default function (props) {
     let { charSet } = props;
     const classes = useStyles();
-    const [char, setChar] = React.useState(randomChar());
+
+    const [char, setChar] = React.useState("");
     const [input, setInput] = React.useState("");
+    const [options, setOptions] = React.useState([]);
     const [showLatin, setShowLatin] = React.useState(false);
+
+    useEffect(() => {
+        init();
+    }, []);
 
     function randomChar() {
         if (charSet === "hangul") {
@@ -52,7 +60,8 @@ export default function (props) {
             let index = parseInt(Math.random() * kana.allChar.length);
             return kana.allChar[index];
         } else if (charSet === "hanja") {
-
+            let index = parseInt(Math.random() * lv1Hanja.length);
+            return lv1Hanja[index];
         }
     }
 
@@ -65,10 +74,38 @@ export default function (props) {
     }
 
     function init() {
+        let char = randomChar();
         setInput("");
-        setChar(randomChar());
+        setChar(char);
+        randomAnsers(char);
         setShowLatin(false);
+
     }
+
+    function randomAnsers(char) {
+        let result = [];
+        let trueAnswer = hanjaMap[char][0];
+        result.push({ opt: trueAnswer, right: true });
+        while (result.length < 4) {
+            let index = parseInt(Math.random() * lv1Hanja.length);
+            let rndHanja = lv1Hanja[index];
+            let opt = hanjaMap[rndHanja][0];
+            if (trueAnswer !== opt) {
+                result.push({ opt: opt, right: false });
+            }
+        }
+
+        var randomNumber = function () {
+            // randomNumber(a,b) 返回的值大于 0 ，则 b 在 a 的前边；
+            // randomNumber(a,b) 返回的值等于 0 ，则a 、b 位置保持不变；
+            // randomNumber(a,b) 返回的值小于 0 ，则 a 在 b 的前边。
+            return 0.5 - Math.random()
+        }
+        result.sort(randomNumber);
+        setOptions(result);
+    }
+
+
 
     function onInput(e) {
         let value = e.target.value;
@@ -85,6 +122,17 @@ export default function (props) {
         }
     }
 
+    function clickOption(answer) {
+        if (answer.right) {
+            setTimeout(() => {
+                init();
+            }, 3000);
+        }
+        answer.select = true;
+        setOptions(JSON.parse(JSON.stringify(options)));
+    }
+
+
     return <Card>
         <CardContent>
             <Typography align="center" className={classes.root}>
@@ -94,7 +142,7 @@ export default function (props) {
                 </span>}
                 {/*- {Aromanize.romanize(char)} */}
             </Typography>
-            <FormControl fullWidth className={classes.margin} variant="outlined">
+            {/* <FormControl fullWidth className={classes.margin} variant="outlined">
                 <InputLabel htmlFor="outlined-adornment-amount">输入发音</InputLabel>
                 <OutlinedInput
                     id="outlined-adornment-amount"
@@ -103,12 +151,22 @@ export default function (props) {
                     labelWidth={60}
                     placeholder="输入发音"
                 />
-            </FormControl>
+            </FormControl> */}
+
+            {
+                options.map(o => <div style={{ width: "100%", textAlign: "center", marginTop: 20 }}>
+                    <Button variant="contained" disabled={o.select && !o.right} color={o.select ? (o.right ? "secondary" : "") : "primary"} style={{ width: "90%" }} onClick={() => { clickOption(o) }}>
+                        {o.opt}
+                    </Button>
+                </div>)
+            }
+
+
         </CardContent>
 
-        <CardActions>
+        {/* <CardActions>
             <Button variant="contained" color="primary" onClick={() => setShowLatin(true)} >显示latin </Button>
-        </CardActions>
+        </CardActions> */}
     </Card>;
 }
 
